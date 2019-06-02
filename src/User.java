addb9dd70c951a7fbfa92f24b90683318c0abeec
Felipe Ribeiro <fesribeiro0@gmail.com>
2019-06-02 19:16:30 -0300

    users config

--- src/User.java
+++ src/User.java
@@ -12,13 +12,14 @@ import javax.faces.context.FacesContext;
 @ManagedBean
 public class User {
 	int id;
-	String endereco;
 	String nome;
 	String sexo;
 	String cpf;
 	String telefone;
+	String endereco;
 	ArrayList usuarios;
 	Connection conn;
+	private Map<String, Object> sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
 
 	
 	public String getEndereco() {
@@ -126,6 +127,84 @@ public class User {
 		return usuarios;
 		
 	}
-
+	
+	public String criarUsuario()
+	{
+		try {
+			conn = getDb();
+			PreparedStatement stmt = conn.prepareStatement("INSERT INTO clientes (nome, cpf, telefone, endereco, sexo) VALUES (?,?,?,?,?)");
+			stmt.setString(1, nome);
+			stmt.setString(2, cpf);
+			stmt.setString(3, telefone);
+			stmt.setString(4, endereco);
+			stmt.setString(5, sexo);
+			stmt.executeUpdate();
+			conn.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		return "clientes.xhtml?faces-redirect=true";
+	}
+	
+	public void removerUsuario(int id)
+	{
+		try {
+			conn = getDb();
+			PreparedStatement stmt = conn.prepareStatement("DELETE FROM clientes where id = " + id);
+			stmt.executeUpdate();
+			conn.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+	}
+	
+	public String alterarUsuario(int id)
+	{
+		User usuario = null;
+		try {
+			conn = getDb();
+			Statement stmt = getDb().createStatement();
+			ResultSet result = stmt.executeQuery("select * from clientes where id = " + id);
+			result.next();
+			usuario = new User();
+			usuario.setId(result.getInt("id"));
+			usuario.setNome(result.getString("nome"));
+			usuario.setEndereco(result.getString("endereco"));
+			usuario.setTelefone(result.getString("telefone"));
+			usuario.setCpf(result.getString("cpf"));
+			usuario.setSexo(result.getString("sexo"));
+			sessionMap.put("edit", usuario);
+			conn.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		return "/editar-usuario.xhtml?faces-redirect=true";
+		
+	}
+	
+	public String alterarDadosUsuario(User edit)
+	{
+		try {
+			conn = getDb();
+			PreparedStatement stmt = conn.prepareStatement("UPDATE clientes SET nome=?, cpf=?, telefone=?, endereco=?, sexo=? where id=?");
+			stmt.setString(1, edit.nome);
+			stmt.setString(2, edit.cpf);
+			stmt.setString(3, edit.telefone);
+			stmt.setString(4, edit.endereco);
+			stmt.setString(5, edit.sexo);
+			stmt.setInt(6, edit.id);
+			stmt.executeUpdate();
+			conn.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		
+		return "clientes.xhtml?faces-redirect=true";
+		
+	}
+	
 }
 
